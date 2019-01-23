588c7c52fe872a69c34869abc1363097cd39a937
Chris Halbersma <chris.halbersma@verizondigitalmedia.com>
2019-01-22 23:29:16 -0800

    Version Dump in Pre Build.

--- travis/pre_build.sh
+++ travis/pre_build.sh
@@ -9,3 +9,7 @@ echo -e "TRAVIS_REPO_SLUG : ${TRAVIS_REPO_SLUG}"
 echo -e "TRAVIS_BRANCH : ${TRAVIS_BRANCH}"
 echo -e "TRAVIS_EVENT_TYPE : ${TRAVIS_EVENT_TYPE}"
 echo -e "Git Branch : $(git rev-parse --abbrev-ref HEAD)"
+
+echo -e "Python Verison : "
+python3 -V
+python -V

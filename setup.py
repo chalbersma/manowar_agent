8bc4b5815d8c38283b3f583d6efe24cc12c83186
Chris Halbersma <chris.halbersma@verizondigitalmedia.com>
2019-01-25 21:05:47 -0800

    Testing new setup.py with travisy things.

--- setup.py
+++ setup.py
@@ -2,9 +2,29 @@
 
 import setuptools
 import sys
+import os
+import git
+
+current_repo = git.Repo()
+
+if current_repo.bare:
+    print("Something went wrong Repo is Bare, Failing the Build.")
+    sys.exit(1)
+else:
+    current_branch = current_repo.active_branch.name
+    travis_repo = os.environ.get("TRAVIS_REPO_SLUG", "NOTRAVIS")
+    travis_branch = os.environ.get("TRAVIS_BRANCH", "UNKNOWN")
+    travis_event_type = os.environ.get("TRAVIS_EVENT_TYPE", "UNKNOWN")
+
+    print(current_branch, travis_repo, travis_branch, travis_event_branch)
+    sys.exit(0)
+
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+# Get Version
 
 setuptools.setup(
     name="stingcell",

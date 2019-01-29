8e452e0bc66150f3583c5b03a4bd6936637bee14
Chris Halbersma <chris.halbersma@verizondigitalmedia.com>
2019-01-28 21:05:06 -0800

    Upload Stuff

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+'''
+An attempt to make a ghetto packaging script.
+'''
+
 import setuptools
 import sys
 import os
 import git
+import subprocess
 
 current_repo = git.Repo()
 
@@ -14,7 +19,7 @@ else:
 
     env_keys = dict(os.environ).keys()
 
-    travis_keys = [ key for key in env_keys if key.startswith("TRAVIS") ]
+    travis_keys = [key for key in env_keys if key.startswith("TRAVIS")]
 
     for key in travis_keys:
         print("{} : {}".format(key, os.environ[key]))
@@ -24,6 +29,7 @@ else:
     travis_branch = os.environ.get("TRAVIS_BRANCH", "UNKNOWN")
     travis_event_type = os.environ.get("TRAVIS_EVENT_TYPE", "UNKNOWN")
     travis_tag = os.environ.get("TRAVIS_TAG", False)
+    travis_build_no = os.environ.get("TRAVIS_BUILD_NUMBER", 0)
 
 # Set Default Version
 version = "0.0.0"
@@ -32,9 +38,26 @@ upload_to_pypi = False
 # My Known Good Repository
 if travis_repo == "chalbersma/manowar_agent" and travis_branch == "master" and travis_tag is not False:
     # Make a Version Fix here that equls the tag
-    print("We're working with a Tag.")
-    print(travis_tag)
+    print("Tagged Branch : {}".format(travis_tag))
+    version = travis_tag
+    upload_to_pypi = "prod"
+elif travis_repo == "chalbersma/manowar_agent":
+    # This is in my repo and
+    version = "0.0.{}".format(travis_build_no)
+    upload_to_pypi = "stag"
+
+if upload_to_pypi is not False and upload_to_pypi == "stag":
+    os.environ["TWINE_USERNAME"] = os.environ.get("PYPI_STAG_UNAME", "whoidit")
+    os.environ["TWINE_PASSWORD"] = os.environ.get("PYPI_STAG_PASSWD", "whasit")
+    twine_cmd = ["twine", "upload", "--repository-url", "https://test.pypi.org/legacy/", "dist/*"]
+elif upload_to_pypi is not False and upload_to_pypi == "stag":
+    os.environ["TWINE_USERNAME"] = os.environ.get("PYPI_PROD_UNAME", "whoidit")
+    os.environ["TWINE_PASSWORD"] = os.environ.get("PYPI_PROD_PASSWD", "whasit")
+    twine_cmd = ["twine", "upload", "dist/*"]
+else:
+    # Not Uploading
     pass
+
 
 
 with open("README.md", "r") as fh:
@@ -44,7 +67,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stingcell",
-    version="0.0.0",
+    version=version,
     author="Chris Halbersma",
     author_email="chris+manowar@halbersma.us",
     description="Package to Add as a Collector",
@@ -54,7 +77,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent"
     ],
@@ -67,3 +90,13 @@ setuptools.setup(
     ],
     scripts=["manowar_saltcell"]
 )
+
+if upload_to_pypi is not False:
+
+    print("Attempting to Upload to PyPi : {}".format(upload_to_pypi))
+
+    result = subprocess.check_call(twine_cmd)
+
+    print("Result : {}".format(result))
+else:
+    print("Not attempting to Upload.")

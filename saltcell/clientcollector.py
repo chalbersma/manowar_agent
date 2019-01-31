ac92d6d8a7095b745e94ef67657e8c8b5df37e47
Chris Halbersma <chris.halbersma@verizondigitalmedia.com>
2019-01-30 19:16:28 -0800

    fix a no ip scenario

--- saltcell/clientcollector.py
+++ saltcell/clientcollector.py
@@ -268,8 +268,8 @@ class Host:
 
         # Get Local Addresses
         ipa_object = list()
-        ipa_object.extend(list(self.collection_info["ipv4_addr"].keys()))
-        ipa_object.extend(list(self.collection_info["ipv6_addr"].keys()))
+        ipa_object.extend(list(self.collection_info.get("ipv4_addr", {}).keys()))
+        ipa_object.extend(list(self.collection_info("ipv6_addr", {}).keys()))
 
 
         self.logger.info("Raw Intel Object for this Host : \n{}".format(ipa_object))
